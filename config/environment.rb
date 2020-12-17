9d323298204d9fb15014bdc3cc685e7ec1b9f456
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-17 23:45:58 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -282,7 +282,7 @@ class MusicLibraryController
   def play_song
    puts "Which song number would you like to play?"
    input = gets.strip.to_i
-   if input.between?(1, Song.all.length)
+   if input.between?(1, Song.all.length) && input != nil
      song = Song.sorted[input-1]
      puts "Playing #{song.name} by #{song.artist.name}"
    end
